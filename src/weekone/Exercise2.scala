66ca5cc8523d4591f82a20f49f7abd3a8c5241a5
Samuel Giles <aphro.cag@googlemail.com>
2012-01-24 20:05:15 +0000

    Problem specification for exercise 2.

---
+++ src/weekone/Exercise2.scala
@@ -0,0 +1,19 @@
+package weekone
+
+/**
+ * Exercise 2:
+ * Write a program to implement the following specification and use it to create a record of and a printout of your Semester 2 timetable.
+ * 
+ * Specification:
+ * 
+ * 	A semester consists of 4 modules. Each module has two sessions. A session can be a lecture, a tutorial or a
+ *	workshop, has a start time (consisting of hours and minutes), a duration (a positive integer), a room (consisting of a
+ *	building code and a room number), a module code (consisting	of a level, course code and module number) and a lecturer who
+ *	has a name.
+ * 
+ */
+object Exercise2 {
+  def main(args: Array[String]) : Unit = {
+    
+  }
+}
