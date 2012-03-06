daaac0ea596e040cf6b8bee07c942cdda4a39bc2
Samuel Giles <aphro.cag@googlemail.com>
2012-03-06 01:33:24 +0000

    Added an improved JSON parser using the repsep combinator

--- src/combinatorparsing/SimpleJSONParser.scala
+++ src/combinatorparsing/SimpleJSONParser.scala
@@ -5,8 +5,13 @@ import scala.util.parsing.combinator._
 object Parser2 {
   def main(args: Array[String]): Unit = {
    val jsonString = """{ "address_book": { "name" : "John Smith", "address": { "street": "100 Some Street", "city": "London", "postcode": "SW1 8AJ"}, "phone_numbers": [ "123458", "0876321"]}}""";
-   val parser = new SimpleJSONParser();
-   println(parser.parseAll(parser.obj, jsonString));
+   val simpleParser = new SimpleJSONParser();
+   val enhancedParser = new EnhancedJSONParser();
+   println("Simple Parser:")
+   println(simpleParser.parseAll(simpleParser.obj, jsonString))
+   println("Enhanced Parser:")
+   println(enhancedParser.parseAll(enhancedParser.obj, jsonString))
+   
   }
 }	
 
@@ -25,4 +30,11 @@ class SimpleJSONParser extends JavaTokenParsers {
 	def members: Parser[Any] = member ~ rep("," ~ member)
 	def member: Parser[Any] = stringLiteral ~ ":" ~ value
 	def values: Parser[Any] = value ~ rep("," ~ value )
+}
+
+class EnhancedJSONParser extends JavaTokenParsers {
+	def value: Parser[Any] = obj | arr | stringLiteral | floatingPointNumber | "null" | "true" | "false"
+	def obj  : Parser[Any] = "{" ~ repsep(member, ",") ~ "}"  // The repsep combinator parses a possibly empty sequence of terms separated by a given separator string.
+	def arr  : Parser[Any] = "[" ~ repsep(value, ",") ~ "]"
+	def member : Parser[Any] = stringLiteral ~ ":" ~ value
 }
